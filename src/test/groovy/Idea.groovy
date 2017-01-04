958569965313425aedff0a8f53a12b53dc30983d
Bruno de Lima <dlimaun@gmail.com>
2017-01-04 07:03:56 -0300

    Update idea to set the maven path

--- src/test/groovy/Idea.groovy
+++ src/test/groovy/Idea.groovy
@@ -4,8 +4,7 @@
 public static void main(String[] args) {
     String userHome = System.getProperty("user.home")
     final File gradleCache = new File(userHome, ".gradle/caches/modules-2/files-2.1/")
-//    final File mavenRepo = new File(userHome, ".m2/repository/")
-    final File mavenRepo = new File(userHome, ".m2t/repository/")
+    final File mavenRepo = new File(userHome, ".m2/repository/")
 
     String[] files = gradleCache.list()
     println "Groups found $files.length"
